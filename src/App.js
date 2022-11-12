874ccc33164590344cbf976f71b6aab1d169b669
Joao Xu <joaoxu@Joaos-MacBook-Air.local>
2022-11-12 17:26:09 +0000

    :(

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import { Joystick } from 'react-joystick-component';
+import Popup from 'reactjs-popup';
 
-const socket = new WebSocket("wss://localhost:1234");
+//const socket = new WebSocket("wss://localhost:1234");
 /*socket.onopen = (event) => {
   socket.send("uhhh random");
 };*/
+
 function handleMove(event) {
   console.log("Move", event);
 }
@@ -13,13 +15,19 @@ function handleStop() {
   console.log("Stop");
 }
 
-
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Test!</h1>
+        <form>
+          <label>
+           Name:
+            <input type="text" name="name" />
+          </label>
+          <input type="submit" value="Submit" />
+          </form>
         <Joystick size={100} baseColor="black" stickColor="blue" move={handleMove} stop={handleStop}></Joystick>
       </header>
     </div>
