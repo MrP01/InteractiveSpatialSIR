da78e3bc9928e2007a854ed2756b476cfc154888
Marta <marta.religa@hotmail.com>
2022-11-12 18:18:39 +0000

    :/

---
+++ simulator/Clients.h
@@ -0,0 +1,30 @@
+// Copyright (C) 2016 Kurt Pattyn <pattyn.kurt@gmail.com>.
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR BSD-3-Clause
+#ifndef Server_H_H
+#define Server_H_H
+
+#include <QtCore/QObject>
+#include <QtCore/QList>
+
+QT_FORWARD_DECLARE_CLASS(QWebSocketServer)
+QT_FORWARD_DECLARE_CLASS(QWebSocket)
+QT_FORWARD_DECLARE_CLASS(QString)
+
+class Server: public QObject
+{
+    Q_OBJECT
+public:
+    explicit Server_H(quint16 port, QObject *parent = nullptr);
+    ~Server_H() override;
+
+private slots:
+    void onNewConnection();
+    void process(const QString &message);
+    void socketDisconnected();
+
+private:
+    QWebSocketServer *m_pWebSocketServer;
+    QList<QWebSocket *> m_clients;
+};
+
+#endif //Server_H_H
